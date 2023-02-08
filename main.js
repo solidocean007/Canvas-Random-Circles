8fd154aefc08164eaf493621b7815f685b79089f
solidocean007 <clintonwilliams007@gmail.com>
2023-02-07 21:46:59 -0500

    Add dynamic resize on circles and colors

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext("2d");
 
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+const maxRadius = 40;
+const minRadius = 2;
+
+const colorArray = ["#025159", "#3F858C", "#7AB8BF", "#C4EEF2", "#A67458"];
+
+window.addEventListener("mousemove", function (event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+});
+
+window.addEventListener("resize", function () {
+  canvas.width = this.window.innerWidth;
+  canvas.height = this.window.innerHeight;
+
+  init();
+});
 
 function Circle(x, y, dx, dy, radius) {
   this.x = x;
@@ -11,13 +32,13 @@ function Circle(x, y, dx, dy, radius) {
   this.dx = dx;
   this.dy = dy;
   this.radius = radius;
+  this.minRadius = radius;
+  this.color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
   this.draw = function () {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-    ctx.lineWidth = 5;
-    ctx.strokeStyle = "blue";
-    ctx.stroke();
+    ctx.fillStyle = this.color;
     ctx.fill();
   };
   this.update = function () {
@@ -30,33 +51,49 @@ function Circle(x, y, dx, dy, radius) {
 
     this.x += this.dx;
     this.y += this.dy;
+
+    // interactivity
+    if (
+      mouse.x - this.x < 50 &&
+      mouse.x - this.x > -50 &&
+      mouse.y - this.y < 50 &&
+      mouse.y - this.y > -50
+    ) {
+      if (this.radius < maxRadius) {
+        this.radius += 1;
+      }
+    } else if (this.radius > this.minRadius) {
+      this.radius -= 1;
+    }
+
     this.draw();
   };
 }
 
-const circleArray = [];
-for (let i = 0; i < 1000; i++) {
-  const radius = 10;
+let circleArray = [];
 
-  let x = Math.random() * (innerWidth - radius * 2) + radius;
-  let y = Math.random() * (innerHeight - radius * 2) + radius;
-  let dx = (Math.random() - 0.5) * 10;
-  let dy = (Math.random() - 0.5) * 10;
-  circleArray.push(new Circle(x, y, dx, dy, radius));
+
+function init() {
+  circleArray = [];
+  for (let i = 0; i < 800; i++) {
+    const radius = Math.random() * 10 + 1;
+    let x = Math.random() * (innerWidth - radius * 2) + radius;
+    let y = Math.random() * (innerHeight - radius * 2) + radius;
+    let dx = Math.random() - 0.5;
+    let dy = Math.random() - 0.5;
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+  }
 }
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, innerWidth, innerHeight);
 
-  for(let i = 0; i < circleArray.length; i++){
+  for (let i = 0; i < circleArray.length; i++) {
     circleArray[i].update();
   }
 }
 
-// animate();
+init()
 
-// for (let i = 0; i < 5; i++) {
-
-//   ctx.fillRect(x, y, 100, 100);
-// }
+animate();
